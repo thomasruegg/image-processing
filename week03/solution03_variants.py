c67b164938f0ffc5c8e3c5e6f3f0f9ee06b73ddf
Thomas Rüegg <thomas_rueegg@icloud.com>
2023-10-05 16:15:03 +0200

    added week03

---
+++ week03/solution03_variants.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Solution Python Excercise Lab 5
+
+Image Filtering
+
+@author: Patrik Müller
+@date:   11.09.2019
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+import gc
+import cv2
+
+# Parameters
+k = 1.2  # highboost coefficient
+m = 10  # neighborhood radius
+n = 2 * m + 1  # filter size
+
+# Load image
+f = plt.imread("../../images/lions.jpg")
+
+# Tranform image to grayscale and float
+if f.ndim > 2:
+    f = np.mean(f, axis=2)
+f = np.array(f, dtype=float)
+
+# %% image filter
+# add black border to image
+
+gc.disable()
+
+def boxfilter_implemented_python_only(f, m):
+    (h, w) = np.shape(f)
+    ft = np.zeros((h + 2 * m, w + 2 * m))
+    ft[m:-m, m:-m] = f
+    ft = np.array(ft, dtype="uint16")
+    # low pass filter
+    (h2, w2) = np.shape(ft)
+    g = np.zeros_like(f, dtype="float")
+    r = np.arange(m, h2 - m)
+    c = np.arange(m, w2 - m)
+    for rm in range(-m, m + 1):
+        for cm in range(-m, m + 1):
+            g += ft[r + rm, :][:, c + cm]
+    return g
+
+
+def boxfilter_by_copying_image_blockwise(f, m):
+    (h, w) = np.shape(f)
+    ft = np.zeros((h + 2 * m, w + 2 * m))
+    ft[m:-m, m:-m] = f
+    ft = np.array(ft, dtype="uint16")
+    # low pass filter
+    (h2, w2) = np.shape(ft)
+    g = np.zeros_like(f, dtype="float")
+    r = np.arange(m, h2 - m)
+    c = np.arange(m, w2 - m)
+    for rm in range(-m, m + 1):
+        for cm in range(-m, m + 1):
+            g += ft[r + rm, :][:, c + cm]
+    return g
+
+
+def boxfilter_by_copying_image_row_and_columnwise(f, m):
+    (h, w) = np.shape(f)
+    ft = np.zeros((h + 2 * m, w + 2 * m))
+    ft[m:-m, m:-m] = f
+    ft = np.array(ft, dtype="uint16")
+    # low pass filter
+    (h2, w2) = np.shape(ft)
+    g = np.zeros((h, w + 2 * m), dtype="float")
+    p = np.zeros((h, w), dtype="float")
+    r = np.arange(m, h2 - m)
+    c = np.arange(m, w2 - m)
+    for rm in range(-m, m + 1): # column wise convolution
+        g += ft[r + rm, :]
+    for cm in range(-m, m + 1): # row wise convolution
+        p += g[:, c + cm]
+    return p 
+
+
+
+
+start1 = time.perf_counter_ns()
+g = boxfilter_by_copying_image_blockwise(f, m)
+stop1 = time.perf_counter_ns()
+print(f'blockwise: runtime = {(stop1-start1)/1e6} ms')
+
+start2 = time.perf_counter_ns()
+h = boxfilter_by_copying_image_row_and_columnwise(f, m)
+stop2 = time.perf_counter_ns()
+print(f'separable: runtime = {(stop2-start2)/1.e6} ms')
+
+start3 = time.perf_counter_ns()
+i = cv2.boxFilter(f, ddepth=-1, ksize=(2*m+1, 2*m+1), borderType=cv2.BORDER_ISOLATED)
+stop3 = time.perf_counter_ns()
+print(f'openCV: runtime = {(stop3-start3)/1e6} ms')
+
+
+plt.imshow(g, cmap='gray')
+plt.show()
+plt.imshow(h, cmap ='gray')
+plt.show()
+plt.imshow(i, cmap ='gray')
+plt.show()
+
+
+
---
+++ week03/template03.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Exercise 3 Template
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Parameter
+N = 2 ** 8
+graylevels = range(N)
+
+# Load image
+img = plt.imread("../../images/lions.jpg")
+
+# Tranform image to grayscale
+f = img.copy()
+if f.ndim > 2:
+    f = np.mean(f, axis=2)
+
+# Calculate normalized histogram
+h = np.zeros_like(graylevels)
+
+# Calculate cumulative probability density function (See np.cumsum)
+c = np.zeros_like(graylevels)
+
+# Display result
+fig, ax = plt.subplots(
+    2, 2, sharex="col", num=1, clear=True, figsize=(15, 10), constrained_layout=True
+)
+ax = ax.T.flatten()
+ax[0].imshow(img, cmap="gray")
+ax[0].title.set_text("Original Image")
+ax[0].axis("off")
+ax[1].imshow(f, cmap="gray")
+ax[1].title.set_text("Grayscale Image")
+ax[1].axis("off")
+ax[2].plot(graylevels, h)
+ax[2].title.set_text("Histogram (Probability Density Function)")
+ax[3].plot(graylevels, c)
+ax[3].title.set_text("Cumulative Probability Density Function")
+for a in ax[2:]:
+    a.grid(b=True)
+    ylim = np.max(a.lines[0].get_ydata())
+    a.axis([0, N - 1, 0, 1.05 * ylim])
+plt.show()
