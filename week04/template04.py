e1db444793ce0a4b838e14d9cb2110e825000f4b
thomasruegg <hello@thomasruegg.com>
2023-10-19 16:40:46 +0200

    done week04

--- week04/template04.py
+++ week04/template04.py
@@ -10,8 +10,12 @@ import numpy as np
 
 # Histogram function
 def histogram(img, levels):
-    # TODO calculate a normalized histogram of the image img
-    pass
+    # calculate a normalized histogram of the image img
+    h = np.zeros(np.size(levels))
+    for level in levels:
+        h[level] = np.sum(img == level)
+    h = h / np.sum(h)
+    return h
 
 
 # Parameter
@@ -19,9 +23,9 @@ N = 2 ** 8
 graylevels = range(N)
 
 # Load image
-img = plt.imread("../../images/lions.jpg")
+img = plt.imread("../images/lions.jpg")
 
-# Tranform image to grayscale
+# Transform image to grayscale
 f = img.copy()
 if f.ndim > 2:
     f = np.mean(img, -1).astype("uint8")
@@ -30,17 +34,25 @@ if f.ndim > 2:
 h = np.zeros(N)
 c = np.zeros_like(h)
 # TODO calculate histogram of f -> h
+h = histogram(f, graylevels)
+c = np.cumsum(h)
+
 # TODO calculate cummulative probability density of f -> c
+T = (N - 1) * np.cumsum(h)
+T = np.array(np.round(T), dtype="uint8")
 
 # Transform image
 g = np.zeros_like(f)
 # TODO apply histogram equalization scheme to f -> g
+g = T[f]
 
 # Calculate histogram of transformed image
 ht = np.zeros_like(h)
 ct = np.zeros_like(h)
 # TODO calculate histogram of g -> ht
+ht = histogram(g, graylevels)
 # TODO calculate cummulative probability density of g -> ct
+ct = np.cumsum(ht)
 
 # Display results
 fig, ax = plt.subplots(
@@ -58,9 +70,9 @@ ax[1, 0].imshow(g, cmap="gray")
 ax[1, 1].plot(graylevels, ht)
 ax[1, 2].plot(graylevels, ct)
 for a, title in zip(
-    ax[:, 1:].flatten(), 2 * ["Normalized Histogram", "Cummulative Probability Density"]
+        ax[:, 1:].flatten(), 2 * ["Normalized Histogram", "Cummulative Probability Density"]
 ):
-    a.grid(b=True)
+    a.grid()
     ylim = 1.05 * np.max(a.lines[0].get_ydata())
     a.axis([0, N - 1, 0, ylim])
     a.set_title(title)
